65f860ec72ee66388f92ecd34aab4362247ea3b9
Aditya Bhoye <adibhoye@gmail.com>
2025-11-05 16:09:22 +0530

    Update Assignment_2.java

--- Assignment_2.java
+++ Assignment_2.java
@@ -1,3 +1,16 @@
+// Movie Recommendation System Optimization 
+// A popular OTT platform, StreamFlix, offers personalized recommendations by sorting movies 
+// based on user preferences, such as IMDB rating, release year, or watch time popularity. 
+// However, during peak hours, sorting large datasets slows down the system. 
+// As a backend engineer, you must: 
+// ● Implement Quicksort to efficiently sort movies based on various user-selected 
+// parameters. 
+// ● Handle large datasets containing of  movies while maintaining fast response times 
+
+//Name:Aditya Bhoye
+//PRN:123B1F010
+//Date:25/07/2025
+
 import java.util.*;
 class Movie {
     String title;
